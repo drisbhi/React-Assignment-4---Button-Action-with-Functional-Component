a38f5e2fe653e5e87624cf07e9d99fd51b60a5d2
sahib bajaj <bajajsaib3@gmail.com>
2021-03-13 13:22:54 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,18 @@
 import React, {Component, useState} from "react";
-import "./../styles/App.css";
 
 function App() {
-
-  const [para, setPara] = useState(" ");
-
-  const handleClick = () =>{
-    setPara(
-      <p>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
-    );
-  };
-
   
+  const [set,Csettime] = useState("");
+  const handleClick = () =>{
+    Csettime(1);
+  }
   return (
     <div id="main">
-      <p id="para">{para}</p>
-      <button id="click" onClick={handleClick}>Click</button>
+      
+    {set && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+      <button id="click" onClick={handleClick} >open</button>
     </div>
   );
-  
 }
 
 
